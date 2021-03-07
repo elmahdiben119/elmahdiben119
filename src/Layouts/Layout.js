2b2c9bf592b31e91975f66e3aca0469ee71a0ad3
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2021-03-07 20:14:43 +0100

    main navbar

--- src/Layouts/Layout.js
+++ src/Layouts/Layout.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-bootstrap';
+import { NavLink,Navbar, Image } from 'react-bootstrap';
+import mypic from '../assets/images/elmahdibenbrahim.jpg'
 export default class Layout extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <NavLink className="navbar-brand" to="/#">
+      <Navbar bg="dark" className="navbar navbar-expand-lg navbar-dark" variant="dark">
+          <Navbar.Brand href="/">
+            <Image
+              alt=""
+              src= { mypic }
+              width="50"
+              height="50"
+              className="d-inline-block align-center"
+              roundedCircle 
+            />{' '}
             El Mahdi Benbrahim
-          </NavLink>
+          </Navbar.Brand>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                  <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/#">
-                        Home
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/#">
-                        Features
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/#">
-                        Pricing
-                      </NavLink>
-                    </li>
-                  </ul>
-                  <NavLink className="btn btn-outline-info my-2 my-sm-0" to="/login">
-                    Login
-                  </NavLink>
-                  <NavLink
-                    className="btn btn-outline-info my-2 my-sm-0 ml-3"
-                    to="/register"
-                  >
-                    Register
-                  </NavLink>
-                </div>
-              </nav>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/#">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/#">
+                  Experience
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/#">
+                  Education
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/#">
+                  Certifications 
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/#">
+                  Skills 
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </Navbar>
     )
   }
 }
