cbbc6ef197c24d68f998ddc6531aaa0295b338b7
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2021-03-12 22:14:12 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Layout from './Layouts/Layout'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -11,7 +10,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
