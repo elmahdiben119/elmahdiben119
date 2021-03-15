6d15d4208fca07b2fdf588023f5bc54aad084ac2
El Mahdi Benbrahim <benbrahim.elmahdi@gmail.com>
2021-03-15 22:38:53 +0100

    fix header & content

--- src/App.js
+++ src/App.js
@@ -1,33 +1,124 @@
-import './App.css';
-import Slider from "react-slick";
-import mypic from './assets/images/elmahdibenbrahim.jpg'
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { Avatar } from '@material-ui/core';
+import mypic from './assets/images/elmahdibenbrahim.jpg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Experiences from './Experiences/Experiences';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+const obj = {
+  firstName: "El Mahdi",
+  lastName: "Benbrahim",
+  age: 22,
+  position: "Laravel Developer",
+  email: "benbrahim.elmahdi@gmail.com",
+  phone: "+33769242104",
+};
+
+const IOSSwitch = withStyles((theme) => ({
+  root: {
+    width: 42,
+    height: 26,
+    padding: 0,
+    margin: theme.spacing(1),
+  },
+  switchBase: {
+    padding: 1,
+    '&$checked': {
+      transform: 'translateX(16px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        backgroundColor: '#000000',
+        opacity: 1,
+        border: 'none',
+      },
+    },
+    '&$focusVisible $thumb': {
+      color: '#52d869',
+      border: '6px solid #fff',
+    },
+  },
+  thumb: {
+    width: 24,
+    height: 24,
+  },
+  track: {
+    borderRadius: 26 / 2,
+    border: `1px solid ${theme.palette.grey[400]}`,
+    backgroundColor: theme.palette.grey[50],
+    opacity: 1,
+    transition: theme.transitions.create(['background-color', 'border']),
+  },
+  checked: {},
+  focusVisible: {},
+}))(({ classes, ...props }) => {
+  return (
+    <Switch
+      focusVisibleClassName={classes.focusVisible}
+      disableRipple
+      classes={{
+        root: classes.root,
+        switchBase: classes.switchBase,
+        thumb: classes.thumb,
+        track: classes.track,
+        checked: classes.checked,
+      }}
+      {...props}
+    />
+  );
+});
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedC: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
   };
   return (
-    <div className="App">
-      <Slider {...settings}>
-        <div>
-          <img width="50" height="50" className="card-img-top" src={ mypic } alt="profile" />
-        </div>
-        <div>
-          <img width="50" height="50" className="card-img-top" src={ mypic } alt="profile" />
-        </div>
-        <div>
-          <img width="50" height="50" className="card-img-top" src={ mypic } alt="profile" />
-        </div>
-        <div>
-          <img width="50" height="50" className="card-img-top" src={ mypic } alt="profile" />
-        </div>
-      </Slider>
-    </div >
+    <div className={classes.root}>
+      <AppBar position="sticky" color="default">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <Avatar alt={obj.firstName + ' ' + obj.lastName} src={mypic} />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {obj.firstName + ' ' + obj.lastName}
+          </Typography>
+          <FormControlLabel
+            control={<IOSSwitch checked={state.checkedB} onChange={handleChange} name="checkedB" />}
+            label="Dark Mode"
+          />
+        </Toolbar>
+      </AppBar>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Experiences></Experiences>
+      </Container>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import 'alpinejs'
 
 //import reportWebVitals from './reportWebVitals';
 
